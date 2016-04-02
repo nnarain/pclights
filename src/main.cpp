e09aa50c431b5b2d4873a68379c5db8a6e69435d
Natesh Narain <nnaraindev@gmail.com>
2016-04-02 03:04:12 -0400

    testing logger

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 
 #include <iostream>
 
+#include <simplelogger/simplelogger.h>
+#include <simplelogger/logger/console_logger.h>
+
+using namespace simplelogger;
+
 int main()
 {
+	registerLogger<ConsoleLogger>();
 
+	for(int i = 0; i < 5; ++i)
+	{
+		log(Level::INFO, "Hello World\n", 12);
+	}
+
+
+	releaseLoggers();
 	return 0;
 }
