2a7a08e7d6fbe0f45c1c93c1c1fd713a8e0e1668
Natesh Narain <nnaraindev@gmail.com>
2016-04-09 03:28:05 -0400

    added sci functions to firmware

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,4 +1,5 @@
 
+#if 1
 #include <avr/io.h>
 #include <util/delay.h>
 
@@ -6,37 +7,87 @@
 
 #include <WS2812/Ws2812Driver.h>
 
-#define RED   0xFF0000
-#define GREEN 0x00FF00
-#define BLUE  0x0000FF
+#include "sci.h"
+
+#define BAUD_RATE(baud) ((F_CPU / 16 / (baud))-1)
 
 GPIO(PORTB, DDRB, GpioB);
 
 typedef Ws2812Driver<GpioB, 5, 60> LedsDriver;
+
+void initSci();
 
 int main()
 {
 	LedsDriver leds;
 	leds.begin();
 
-	uint32_t colors[] = { RED, RED | GREEN, GREEN, GREEN | BLUE, BLUE, BLUE | RED };
-	int colors_len = 6;
-
-	leds.clear();
-	leds.show();
+	initSci();
 
 	for(;;)
 	{
-		for(int i = 0; i < colors_len; ++i)
-		{
-			for(int j = 0; j < LedsDriver::NUM_LEDS; ++j)
-			{
-				leds.setPixel(j, colors[i]);
-				leds.show();
-				_delay_ms(100);
-			}
-		}
+		char c = sci::getc();
+		sci::putc(c);
 	}
 
 	return 0;
 }
+
+void initSci()
+{
+	// enable rx and tx
+	sci::rx_enable::set();
+	sci::tx_enable::set();
+
+	// frame format
+	sci::frame_format::write(sci::frame_options::DATA8);
+	sci::stop_bits_1::set();
+
+	// set baud rate
+	sci::baud::write((uint16_t)BAUD_RATE(9600));
+}
+
+#endif
+
+#if 0
+
+#include <avr/io.h>
+#include "sci.h"
+ 
+#define USART_BAUDRATE 9600
+#define BAUD_PRESCALE (((F_CPU/(USART_BAUDRATE*16UL)))-1)
+ 
+int main(void)
+{
+	char recieved_byte;
+
+//	UCSR0B |= (1<<RXEN0)  | (1<<TXEN0);
+	sci::rx_enable::set();
+	sci::tx_enable::set();
+//	UCSR0C |= (1<<UCSZ00) | (1<<UCSZ01);
+	sci::frame_format::write(sci::frame_options::DATA8);
+	sci::stop_bits_1::set();
+/*	UBRR0H  = (BAUD_PRESCALE >> 8);
+	UBRR0L  = BAUD_PRESCALE;*/
+	sci::baud::write((uint16_t)BAUD_RATE(9600));
+
+	for(;;)
+	{
+		// wait until a byte is ready to read
+	//	while( ( UCSR0A & ( 1 << RXC0 ) ) == 0 ){}
+
+		// grab the byte from the serial port
+	//	recieved_byte = UDR0;
+		recieved_byte = sci::getc();
+
+		// wait until the port is ready to be written to
+	//	while( ( UCSR0A & ( 1 << UDRE0 ) ) == 0 ){}
+
+		// write the byte to the serial port
+	//	UDR0 = recieved_byte;
+		sci::putc(recieved_byte);
+	}
+	return 0;   /* never reached */
+}
+
+#endif
