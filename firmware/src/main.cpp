6204f5fc28b158ee9e67d66f5a903a6e8a7c9cd0
Natesh Narain <nnaraindev@gmail.com>
2016-08-21 02:19:02 -0400

    basic command light strip

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -9,10 +9,62 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
+#include "SerialStream.h"
+
 #define PIN 6
 #define NUM_PIXELS 60
 
+#define SIGNATURE ((('P' & 0xFFFF) << 8) | 'L')
+
+enum Command
+{
+    CLEAR = 0,
+    SET_COLOR = 1
+};
+
+struct Header
+{
+    uint16_t signature;
+    uint8_t command;
+    uint16_t payload_length;
+};
+
 static Adafruit_NeoPixel pixels(NUM_PIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void clear()
+{
+    for(int i = 0; i < NUM_PIXELS; ++i)
+    {
+        pixels.setPixelColor(i, 0, 0, 0);
+    }
+
+    pixels.show();
+}
+
+void setColor()
+{
+    char buffer[3];
+    Serial.readBytes(buffer, 3);
+
+    for(int i = 0; i < NUM_PIXELS; ++i)
+    {
+        pixels.setPixelColor(i, buffer[0], buffer[1], buffer[2]);
+    }
+
+    pixels.show();
+}
+
+Header readHeader()
+{
+    Header header;
+    char buffer[sizeof(Header)];
+    Serial.readBytes(buffer, sizeof(Header));
+
+    SerialStream ss(buffer);
+    ss >> header.signature >> header.command >> header.payload_length;
+
+    return header;
+}
 
 void setup()
 {
@@ -26,18 +78,23 @@ void setup()
 
 void loop()
 {
-    if(Serial.available() >= 3)
+    // check if a header is available
+    if((unsigned int)Serial.available() >= sizeof(Header))
     {
-        char buffer[3];
-        Serial.readBytes(buffer, 3);
+        // get the header
+        Header header = readHeader();
 
-        if(buffer[0] == 1 && buffer[1] == 2 && buffer[2] == 3)
+        // verify the signature is correct
+        if(header.signature == SIGNATURE)
         {
-            for(int i = 0; i < NUM_PIXELS; ++i)
+            switch(header.command)
             {
-                pixels.setPixelColor(i, 0, 255, 0);
-                pixels.show();
-                delay(50);
+                case CLEAR:
+                    clear();
+                    break;
+                case SET_COLOR:
+                    setColor();
+                    break;
             }
         }
     }
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
 
 #include <iostream>
 #include <string>
+#include <thread>
+#include <chrono>
 
 #include <boost/asio.hpp>
 
 #include "serial.h"
+
+void delay(unsigned int ms)
+{
+	std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
+
+void clear(Serial& comm)
+{
+	uint8_t data[] = { 'P', 'L', 0, 0, 0 };
+	comm.write(data, 5);
+}
+
+void setColor(Serial& comm, uint8_t r, uint8_t g, uint8_t b)
+{
+	uint8_t data[] = { 'P', 'L', 1, 0, 3, r, g, b };
+	comm.write(data, 8);
+}
 
 int main(int argc, char * argv[])
 {
@@ -13,13 +32,16 @@ int main(int argc, char * argv[])
 	std::string device_name = "COM4";
 	unsigned int baud_rate = 9600;
 
-	Serial comm_(io, device_name, baud_rate);
-	uint8_t data[] = { 1, 2, 3 };
+	Serial comm(io, device_name, baud_rate);
 
-	std::cout << "-- sending a byte to the mcu" << std::endl;
-	comm_.write(data, 3);
+	clear(comm);
+	delay(1000);
+	setColor(comm, 255, 0, 0);
+	delay(1000);
+	clear(comm);
+	delay(1000);
+	setColor(comm, 0, 255, 0);
 
-	std::cout << "-- finished writing to device" << std::endl;
 
     return 0;
 }
